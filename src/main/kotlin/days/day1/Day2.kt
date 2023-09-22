0b9f68b2206cd9d3cd5ec080124ec94843989334
Stenz123 <michael.stenz2006@gmail.com>
2023-09-22 08:59:56 +0200

    day2p2

--- src/main/kotlin/days/day1/Day2.kt
+++ src/main/kotlin/days/day1/Day2.kt
@@ -16,6 +16,14 @@ class Day2: Day() {
     }
 
     override fun partTwo(): Any {
-        TODO()
+        val inputs = readInput().map {it.split("[- :]".toRegex())}
+        var resultCounter = 0
+        for (password in inputs) {
+            if ((password[4][password[0].toInt()-1] == password[2][0])
+                xor (password[4][password[1].toInt()-1] == password[2][0])) {
+                resultCounter++
+            }
+        }
+        return resultCounter
     }
 }
