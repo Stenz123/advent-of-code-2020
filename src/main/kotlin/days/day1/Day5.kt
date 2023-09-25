36ea2c1ce3f669384a0e0e1f25badc9e18690737
Stenz123 <michael.stenz2006@gmail.com>
2023-09-25 15:20:04 +0200

    day5p2

--- src/main/kotlin/days/day1/Day5.kt
+++ src/main/kotlin/days/day1/Day5.kt
@@ -21,6 +21,24 @@ class Day5: Day(){
     }
 
     override fun partTwo(): Any {
-        TODO("Not yet implemented")
+        val input = readInput().map {
+            it.replace('F','0')
+                .replace('B','1')
+                .replace('R','1')
+                .replace('L','0')
+        }
+        val seatIds = mutableListOf<Int>()
+        for (line in input) {
+            seatIds.add(line.substring(0,7).toInt(2)*8+line.substring(7,10).toInt(2))
+        }
+        seatIds.sort()
+        for (i in seatIds.indices) {
+
+            if (seatIds[i] != seatIds[i+1] - 1) {
+                return seatIds[i]+1
+            }
+        }
+
+        return -1
     }
 }
